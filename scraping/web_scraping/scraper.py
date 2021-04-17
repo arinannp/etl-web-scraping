8c8b5b55ea56d8ec3a3c6ef1a8aaaa26dc2ff779
Arinan Najah Putra <34894631+arinannp@users.noreply.github.com>
2021-04-17 20:30:24 +0700

    Add files via upload

---
+++ scraping/web_scraping/scraper.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import logging
+from typing import List
+
+logging.basicConfig(level=logging.INFO)
+
+
+def scrape(url:str, header:str = None) -> List[pd.DataFrame]:
+    logging.info(f"Scraping website with tag-html table, url: '{url}' ...")
+    return pd.read_html(url, header=header)
